05f30471c20e433a52ddc4b7d6aa886342093542
Mak Patil <makrandapatil17@gmail.com>
2019-04-22 00:10:19 -0700

    readme added

---
+++ src/main/java/com/springboot/docker/api/springbootdockerapi/SpringBootDockerApiApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.docker.api.springbootdockerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDockerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDockerApiApplication.class, args);
+	}
+
+}
